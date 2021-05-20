dd03813af35c9d9dbb67461d8257b4d2471eb795
do <18520604@gm.uit.edu.vn>
2021-05-20 10:43:36 +0700

    fix slider

--- testproject/app/src/main/java/com/example/testproject/Activity/MainActivity.java
+++ testproject/app/src/main/java/com/example/testproject/Activity/MainActivity.java
@@ -3,64 +3,79 @@ package com.example.testproject.Activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
 
-import com.example.testproject.Fragment.AccountFragment;
-import com.example.testproject.Fragment.HomeFragment;
-import com.example.testproject.Fragment.MenuFragment;
-import com.example.testproject.Fragment.ReservationFragment;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.testproject.Adapter.ProductAdapter;
+import com.example.testproject.HomeFragment;
+import com.example.testproject.Model.Product;
 import com.example.testproject.R;
+import com.example.testproject.Untils.Server;
+import com.example.testproject.reservationFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
-    private Fragment fragment;
-    private FragmentManager fragmentManager;
-    private FragmentTransaction fragmentTransaction;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         bottomNavigationView = findViewById(R.id.bottom_nav);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
 
-       getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
 
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod =
-        new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                Fragment fragment = null;
+                    Fragment fragment = null;
 
-                switch (item.getItemId()) {
-                    case R.id.home:
-                        fragment = new HomeFragment();
-                        break;
-                    case R.id.menu:
-                        fragment = new MenuFragment();
-                        break;
-                    case R.id.reservation:
-                        fragment = new ReservationFragment();
-                        break;
-                    case R.id.account:
-                        fragment = new AccountFragment();
-                        break;
+                    switch (item.getItemId()) {
+                        case R.id.home:
+                            fragment = new HomeFragment();
+                            break;
+                        case R.id.menu:
+                            fragment = new MenuFragment();
+                            break;
+                        case R.id.reservation:
+                            fragment = new ReservationFragment();
+                            break;
+                        case R.id.account:
+                            fragment = new AccountFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+
+                    return true;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
-
-                return true;
-            }
-        };
+            };
 
 }
