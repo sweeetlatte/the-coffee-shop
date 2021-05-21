3f9d9866ccf9a8d51998bbd61ede52e9ea87f1c4
Nguyễn Văn Đô <72346938+Dondo7@users.noreply.github.com>
2021-05-21 22:10:14 +0700

    Tạo Login

--- testproject/app/src/main/java/com/example/testproject/Activity/MainActivity.java
+++ testproject/app/src/main/java/com/example/testproject/Activity/MainActivity.java
@@ -3,57 +3,64 @@ package com.example.testproject.Activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.testproject.Fragment.AccountFragment;
 import com.example.testproject.Fragment.HomeFragment;
+import com.example.testproject.Fragment.MenuFragment;
+import com.example.testproject.Fragment.ReservationFragment;
 import com.example.testproject.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
-
-
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         bottomNavigationView = findViewById(R.id.bottom_nav);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+       getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
 
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                    Fragment fragment = null;
+                Fragment fragment = null;
 
-                    switch (item.getItemId()) {
-                        case R.id.home:
-                            fragment = new HomeFragment();
-                            break;
-                        case R.id.menu:
-                            fragment = new MenuFragment();
-                            break;
-                        case R.id.reservation:
-                            fragment = new ReservationFragment();
-                            break;
-                        case R.id.account:
-                            fragment = new AccountFragment();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
-
-                    return true;
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.menu:
+                        fragment = new MenuFragment();
+                        break;
+                    case R.id.reservation:
+                        fragment = new ReservationFragment();
+                        break;
+                    case R.id.account:
+                        fragment = new AccountFragment();
+                        break;
                 }
-            };
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+
+                return true;
+            }
+        };
 
 }
