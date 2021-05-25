cd8d7e84239da02e17039af76fcd58539269c15e
sweetcapucino <thuylinh271055@gmail.com>
2021-05-25 16:15:12 +0700

    back arrow

--- testproject/app/src/main/java/com/example/testproject/Activity/ProductDetailActivity.java
+++ testproject/app/src/main/java/com/example/testproject/Activity/ProductDetailActivity.java
@@ -4,16 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.testproject.Fragment.HomeFragment;
 import com.example.testproject.R;
 
-public class ProductDetailActivity extends Activity {
+public class ProductDetailActivity extends AppCompatActivity {
     TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_detail);
 
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
