150cbdffbf6c64f66abcda94fbbd881d3773878f
sweetcapucino <thuylinh271055@gmail.com>
2021-06-01 11:48:54 +0700

    layout profile done

--- connectMySQL/TheCoffeeShop/getCoffeeProduct.php
+++ connectMySQL/TheCoffeeShop/getCoffeeProduct.php
@@ -4,6 +4,7 @@
     $data = mysqli_query($connection, $query);
     $productList = array();
 
+    JSONARRAY array=new JSONARRAY(response);
     while($row = mysqli_fetch_assoc($data)){
         array_push($productList, new Product(
             $row['MaMon'],
