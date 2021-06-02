1a64854c49e91f72f58b97b30632f6f882aadac5
sweetcapucino <thuylinh271055@gmail.com>
2021-06-02 11:36:32 +0700

    layout reservation done

--- testproject/app/src/main/java/com/example/testproject/Fragment/ReservationFragment.java
+++ testproject/app/src/main/java/com/example/testproject/Fragment/ReservationFragment.java
@@ -1,8 +1,28 @@
 package com.example.testproject.Fragment;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.testproject.R;
+
+import org.jetbrains.annotations.NotNull;
+
 public class ReservationFragment extends Fragment {
+    View view;
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        view = inflater.inflate(R.layout.fragment_reservation, container, false);
+        return view;
+    }
 }
