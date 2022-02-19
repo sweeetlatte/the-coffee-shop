b8242fc6e70cb63ad7887c8511f529e51da64726
sweetcapucino <thuylinh271055@gmail.com>
2022-02-19 17:59:48 +0700

    edit localhost

--- testproject/app/src/main/java/com/example/testproject/Untils/Server.java
+++ testproject/app/src/main/java/com/example/testproject/Untils/Server.java
@@ -1,7 +1,7 @@
 package com.example.testproject.Untils;
 
 public class Server {
-    public static String localhost = "192.168.1.2:8080";
+    public static String localhost = "192.168.1.9:8080";
     public static String pathLogin = "http://" + localhost + "/TheCoffeeShop/getUser.php";
     public static String pathGetNewProduct = "http://" + localhost + "/TheCoffeeShop/getNewProduct.php";
     public static String pathGetCoffeeProduct = "http://" + localhost + "/TheCoffeeShop/getCoffeeProduct.php";
